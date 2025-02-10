d1d513a21e44874a7694162674ad6291f1ec0a71
FajarOfficial199 <fajarshidik709@gmail.com>
2025-02-11 00:22:49 +0700

    Update myapikey.js

--- apikey/myapikey.js
+++ apikey/myapikey.js
@@ -1,19 +1,22 @@
-const users = new Map();
+const users = new Map(); // Menyimpan data pengguna dalam memori
 
+// Fungsi untuk membuat API key baru dengan format "fjr" + angka random
 function generateApiKey() {
     return "fjr" + Math.floor(100000 + Math.random() * 900000); // Contoh: fjr123456
 }
 
 async function addUser(userId) {
-    // Jika pengguna sudah ada, kembalikan API key yang sama
     if (users.has(userId)) {
         return users.get(userId);
     }
 
-    // Buat API key baru dan simpan
     const newApiKey = generateApiKey();
     users.set(userId, newApiKey);
     return newApiKey;
 }
 
-module.exports = addUser
+function checkApiKey(apikey) {
+    return [...users.values()].includes(apikey);
+}
+
+module.exports = { users, generateApiKey, addUser, checkApiKey };
