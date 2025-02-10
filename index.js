87778e1701b6a2364094e3a17c8066ae7b5b08fe
FajarOfficial199 <fajarshidik709@gmail.com>
2025-02-10 23:11:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -157,33 +157,6 @@ app.get("/api/downloader/spotifys", async (req, res) => {
     }
 });
 
-try {
-const requestData = {
-operation: "chatExecute",
-params: {
-text: text,
-languageId: "6094f9b4addddd000c04c94b",
-toneId: "60572a649bdd4272b8fe358c",
-voiceId: ""
-}
-};
-
-const config = {
-headers: {
-Accept: "application/json, text/plain, */*",
-Authentication: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2MTZjMjFhMGE1NTNiNjE1MDhmNWIxOSIsImlhdCI6MTcxMjc2NzUxNH0.qseE0iNl-4bZrpQoB-zxVsc-pz13l3JOKkg4u6Y08OY",
-"Content-Type": "application/json"
-}
-};
-let {data} = await axios.post("https://api.rytr.me/", requestData, config)
-data.data.content = data.data.content.replace(/<\/?p[^>]*>/g, '');
-res.json(data);
-} catch (error) {
-console.error(error);
-res.status(500).send("Internal Server Error");
-}
-});
-
 app.use((req, res, next) => {
   res.status(404).send("Halaman tidak ditemukan");
 });
